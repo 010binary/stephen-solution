689d2bf684b0c079d1b45d3b8fc1125b50ed8591
Okechukwu Daniel <okechukwudaniel12@gmail.com>
2025-02-01 03:48:49 +0100

    commit

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Navigation from "../navigation";
 import InteractiveSection from "./interactive-section";
 import { Button } from "@/components/ui/button";
@@ -14,29 +14,44 @@ export default function Hero() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prev) => (prev + 1) % backgroundImages.length);
-    }, 4000);
+    }, 10000);
 
     return () => clearInterval(interval);
   }, []);
 
+  const zoomInContainer = {
+    initial: { scale: 100 },
+    animate: { scale: 100 },
+  };
+
   return (
-    <div className="relative min-h-[95dvh] max-h-screen">
+    <div className="relative min-h-[95dvh] overflow-hidden  max-h-screen">
       {/* Background Images */}
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={currentImageIndex}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 2 }}
-          className="absolute inset-0 z-0 "
-          style={{
-            backgroundImage: `url(${backgroundImages[currentImageIndex]})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-      </AnimatePresence>
+      {backgroundImages.map((image, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={zoomInContainer}
+            initial={{ opacity: 1 }}
+            animate={{
+              opacity: currentImageIndex === index ? 1 : 0,
+              scale: currentImageIndex === index ? 1.3 : 1,
+            }}
+            exit={{ opacity: 0 }}
+            // transition={{ duration: 5 }}
+            className={`absolute inset-0 z-0 transition-all ease-in-out `}
+            style={{
+              backgroundImage: `url(${image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            transition={{
+              opacity: { duration: 5, delay: 0 },  // Opacity changes immediately
+              scale: { duration: 3, delay: 3 },    // Scale starts after 1 second
+            }}
+          />
+        );
+      })}
 
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/50 z-10" />
@@ -45,14 +60,13 @@ export default function Hero() {
       <div className="relative z-20">
         <Navigation />
 
-        <div className="max-w-[95rem] px-6 mx-auto  pt-24 pb-16 flex justify-between flex-col">
+        <div className="max-w-[95rem] px-6 mx-auto pt-24 pb-16 flex justify-between flex-col">
           <div className="max-w-3xl">
             <h1 className="text-4xl md:text-7xl font-bold text-white mb-10">
               Meeting Future Demand In A Sustainable Way
             </h1>
             <p className="text-lg md:text-3xl text-gray-200 mb-10">
-              We&apos;re doing our part in that regard with greener practices
-              that don&apos;t harm the environment.
+              We&apos;re doing our part in that regard with greener practices that don&apos;t harm the environment.
             </p>
             <Button className="bg-yellow-300 text-black hover:bg-[#d4ea23] duration-300 rounded-none w-72 h-16 text-lg md:text-xl">
               <span className="inline">
@@ -63,13 +77,9 @@ export default function Hero() {
           </div>
 
           {/* Interactive Sections */}
-          <div className="mt-36 grid  md:grid-cols-2 lg:grid-cols-3">
+          <div className="mt-36 grid md:grid-cols-2 lg:grid-cols-3">
             {sections.map((section) => (
-              <InteractiveSection
-                key={section.title}
-                title={section.title}
-                description={section.description}
-              />
+              <InteractiveSection key={section.title} title={section.title} description={section.description} />
             ))}
           </div>
         </div>
